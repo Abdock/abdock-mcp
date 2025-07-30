986c2f1fdd6e42a70c1133e98c708279f8fe5978
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2025-07-30 19:26:59 +0500

    added method for DI

---
+++ src/Backend/Abdock.MCP/WeatherClient.WeatherApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WeatherClient.Abstractions;
+using WeatherClient.WeatherApi.Options;
+
+namespace WeatherClient.WeatherApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWeatherApi(this IServiceCollection services, Action<WeatherApiOptions> configureOptions)
+    {
+        services.Configure(configureOptions);
+        services.AddSingleton<IWeatherClient, WeatherApiClient>();
+        services.AddHttpClient<WeatherApiClient>();
+        return services;
+    }
+}
